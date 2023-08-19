bc0f69dca625888ba13ee847cf347e4ef3da809f
Ivan Podyukov <ivan2003vp@mail.ru>
2023-08-19 23:56:56 +0300

    change #3 settings.py

--- onlineStore/onlineStore/settings.py
+++ onlineStore/onlineStore/settings.py
@@ -114,13 +114,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-if not DEBUG:
-    STATIC_ROOT = ''
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'uploads'
 
